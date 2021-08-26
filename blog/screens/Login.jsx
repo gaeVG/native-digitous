490d80ed4fa1bb008c2b5ffdef4af200ca4b5422
Gaev Cenne <gaev@evilparadise.com>
2021-08-26 17:25:59 +0200

    update: add loggin form et check

--- blog/screens/Login.jsx
+++ blog/screens/Login.jsx
@@ -1,35 +1,61 @@
 import * as React from "react"
 import { useEffect, useState } from "react"
-import { SafeAreaView, StyleSheet, TextInput, Text } from "react-native"
+import { Alert, Button, SafeAreaView, StyleSheet, TextInput, Text } from "react-native"
 
 const styles = StyleSheet.create({
-	login: {
+	view: {
 		marginTop: 50,
 		marginBottom: 10,
-		marginLeft: 20
+		marginLeft: 30,
+		marginRight: 30
 	},
 	input: {
 		height: 40,
-		marginLeft: 20,
-		marginRight: 20,
 		borderWidth: 1,
+		marginTop: 10,
+		marginBottom: 10,
 		padding: 10,
 	},
   });
 
-const Login = () => {
+const Login = props => {
 
 	const [username, onChangeUsername] = useState("")
 
+	const logUser = () => {
+
+		const checkUser = async () => {
+
+			let fetchUsers =await fetch("https://jsonplaceholder.typicode.com/users")
+			fetchUsers = await fetchUsers.json()
+			
+			fetchUsers.map(user => {
+
+				username === user.username
+				?
+					props.onLogin(true)
+				:
+					null
+			})
+		}
+
+		checkUser()
+	}
+
 	return (
 		
-		<SafeAreaView>
-			<Text style={styles.login}>Login :</Text>
+		<SafeAreaView style={styles.view}>
+			<Text >Login :</Text>
 			<TextInput
 				style={styles.input}
 				onChangeText={onChangeUsername}
 				value={username}
 				placeholder="Nom d'utilisateur"
+			/>
+			<Button
+				style={styles.button}
+				title="Connexion"
+				onPress={logUser}
 			/>
 		</SafeAreaView>
 	)
